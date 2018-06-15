93ced199d2b1bec2691433b75c3469b56daba231
enablefzm <gomyhome@foxmail.com>
2018-06-15 20:37:52 +0800

    增加通过ID来获取玩家对象

--- games/players/players.go
+++ games/players/players.go
@@ -7,10 +7,12 @@ import (
 )
 
 var ptManagePlayer *ManagePlayer = &ManagePlayer{
-	mpLoginPlayer: make(map[string]servers.Player, 1000),
-	chAdd:         make(chan servers.Player, 1000),
-	chMove:        make(chan servers.Player, 1000),
-	chGet:         make(chan *proGet, 1000),
+	mpLoginPlayer:     make(map[string]servers.Player, 1000),
+	mpLoginPlayerOnID: make(map[uint]servers.Player, 1000),
+	chAdd:             make(chan servers.Player, 1000),
+	chMove:            make(chan servers.Player, 1000),
+	chGet:             make(chan *proGet, 1000),
+	chGetID:           make(chan *proGetOnID, 1000),
 }
 
 func init() {
@@ -28,9 +30,23 @@ func MovePlayer(p servers.Player) {
 func GetPlayerOnUid(uid string) (servers.Player, bool) {
 	result := &proGet{
 		uid:      uid,
-		chResult: make(chan servers.Player, 0),
+		chResult: make(chan servers.Player, 2),
 	}
 	ptManagePlayer.chGet <- result
+	p := <-result.chResult
+	if p != nil {
+		return p, true
+	} else {
+		return p, false
+	}
+}
+
+func GetPlayerOnId(id uint) (servers.Player, bool) {
+	result := &proGetOnID{
+		id:       id,
+		chResult: make(chan servers.Player, 2),
+	}
+	ptManagePlayer.chGetID <- result
 	p := <-result.chResult
 	if p != nil {
 		return p, true
@@ -48,11 +64,18 @@ type proGet struct {
 	chResult chan servers.Player
 }
 
+type proGetOnID struct {
+	id       uint
+	chResult chan servers.Player
+}
+
 type ManagePlayer struct {
-	mpLoginPlayer map[string]servers.Player
-	chAdd         chan servers.Player
-	chMove        chan servers.Player
-	chGet         chan *proGet
+	mpLoginPlayer     map[string]servers.Player
+	mpLoginPlayerOnID map[uint]servers.Player
+	chAdd             chan servers.Player
+	chMove            chan servers.Player
+	chGet             chan *proGet
+	chGetID           chan *proGetOnID
 }
 
 func (this *ManagePlayer) Run() {
@@ -66,10 +89,12 @@ func (this *ManagePlayer) Run() {
 				oldP.GetCONN().Close()
 			}
 			this.mpLoginPlayer[p.GetUID()] = p
+			this.mpLoginPlayerOnID[p.GetID()] = p
 		case p := <-this.chMove:
 			if oldP, ok := this.mpLoginPlayer[p.GetUID()]; ok {
 				if oldP == p {
 					delete(this.mpLoginPlayer, p.GetUID())
+					delete(this.mpLoginPlayerOnID, p.GetID())
 				}
 			}
 		case pro := <-this.chGet:
@@ -78,6 +103,12 @@ func (this *ManagePlayer) Run() {
 				pro.chResult <- p
 			} else {
 				pro.chResult <- nil
+			}
+		case proId := <-this.chGetID:
+			if p, ok := this.mpLoginPlayerOnID[proId.id]; ok {
+				proId.chResult <- p
+			} else {
+				proId.chResult <- nil
 			}
 		}
 	}
