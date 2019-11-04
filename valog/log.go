3f72a033caba59f31aff2a26f8860bed9845fb04
enablefzm <gomyhome@foxmail.com>
2019-11-04 19:03:51 +0800

    增加LogV2信息

--- valog/log.go
+++ valog/log.go
@@ -18,3 +18,7 @@ func (this *Log) LogMessage(strLog string) {
 func GetNowTime() string {
 	return time.Now().Format("2006-01-02 15:04:05")
 }
+
+func (this *Log) LogMessageV2(logType int32, strLog string) {
+	fmt.Println(fmt.Sprintf("【LOG - %d】 %s %s", logType, GetNowTime(), strLog))
+}
