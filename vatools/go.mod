51e4d0167fa36a6ef1b4760cdf7cd2a3b012eade
enablefzm <gomyhome@foxmail.com>
2021-04-03 13:13:01 +0800

    增加指定时间完成量

--- vatools/go.mod
+++ vatools/go.mod
@@ -1,3 +1,3 @@
 module vatools
 
-go 1.14
+go 1.16
--- vatools/optime.go
+++ vatools/optime.go
@@ -4,12 +4,6 @@ import (
 	"time"
 )
 
-// type OpTime struct {
-// 	lastTime int64
-// }
-
-// func
-
 // 计算过去的时间
 //	@return
 //		int64 记录最后计算的时间
@@ -17,11 +11,20 @@ import (
 func OpTime(lastTime int64, timeSpeed int64) (int64, int) {
 	// 获取当前时间
 	nowTime := time.Now().Unix()
+	return OpAppointTime(lastTime, timeSpeed, nowTime)
+}
+
+// 计算指定截止的时间来可以获得多少个单位
+//	@params
+//		lastTime	int64	上一次的时间
+//		timeSpeed	int64	需要多久时间生成一个单位
+//		appoint		int64	截止计算的时间
+func OpAppointTime(lastTime, timeSpeed, appoint int64) (int64, int) {
 	if timeSpeed < 1 {
 		return lastTime, 0
 	}
 	// 计算过去时间
-	passTime := nowTime - lastTime
+	passTime := appoint - lastTime
 	// 如果达不到需要的时间就不需计算
 	if passTime < timeSpeed {
 		return lastTime, 0
@@ -33,7 +36,7 @@ func OpTime(lastTime int64, timeSpeed int64) (int64, int) {
 	}
 	// 计算剩余时间
 	surplus := passTime % timeSpeed
-	resultLastTime := nowTime - surplus
+	resultLastTime := appoint - surplus
 	// 返回结果
 	return resultLastTime, opValue
 }
